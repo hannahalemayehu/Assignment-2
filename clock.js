64f145ede373dfb82bbc6dc27ce32cc5d6e43cbf
Hannah Alemayehu <hannahrub156@gmail.com>
2020-07-21 16:00:36 +0100

    weather

--- clock.js
+++ clock.js
@@ -3,20 +3,21 @@ function currentTime() {
     var hour = date.getHours();
     var min = date.getMinutes();
     var sec = date.getSeconds();
-    hour = updateTime(hour);
-    min = updateTime(min);
-    sec = updateTime(sec);
-    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
+    hour = updateTime(hour); /*hour*/
+    min = updateTime(min); /*minutes*/
+    sec = updateTime(sec); /*seconds*/
+    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time  */
       var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
   }
   
-  function updateTime(k) {
-    if (k < 10) {
-      return "0" + k;
+  function updateTime(h) {
+    if (h < 10) {
+      return "0" + h;
     }
     else {
-      return k;
+      return h;
     }
   }
   
   currentTime(); 
+  /* inspristion source: https://www.codesdope.com/blog/article/how-to-create-a-digital-clock-using-javascript/ */
---
+++ date.js
@@ -0,0 +1,6 @@
+var todaydate = new Date();
+var day =todaydate.getDate(); /*day*/
+var month = todaydate.getMonth()+1;/*month*/
+var year = todaydate.getFullYear(); /*year*/
+document.write(day + "/" + month + "/" + year) ;
+ 
